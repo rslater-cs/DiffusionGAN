b5e48a3f70c45f422eefa01fb7a53b35f7b93c56
Ryan Slater <ryan.slater157@gmail.com>
2023-05-09 12:22:16 +0100

    Update enviroment_setup.sh

--- environment/shell_scripts/enviroment_setup.sh
+++ environment/shell_scripts/enviroment_setup.sh
@@ -2,4 +2,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p miniconda3 -s -u
 rm Miniconda3-latest-Linux-x86_64.sh
 conda=miniconda3/bin/conda
-$conda env create -f '/vol/research/COM3025Team5/DiffusionGAN/environment/ubuntu_environment.yml'
+$conda env create -f '/vol/research/COM3025Team5/DiffusionGAN/environment/uniplat_environment.yml'
